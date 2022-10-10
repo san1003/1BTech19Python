b5a073e3194ae5b05abe3d25275b719c0f9ac15f
1B6sh <114583306+san1003@users.noreply.github.com>
2022-10-10 10:21:08 +0000

    zrobiono do 4 zadania na karcie pracy  2

---
+++ KartaPracy2.py
@@ -0,0 +1,25 @@
+# Zad1
+# n=int(input())
+
+# if n % 3 == 0:
+# print("TAK")
+# else:
+# print("NIE")
+
+# Zad 2
+# n = int(input())
+# if n >= 100 and n < 1000 and n % 17 == 0:
+#   print ("TAK")
+# else:
+#    print("NIE")
+
+# Zad 3
+
+
+# Zad 4
+LIMIT = 20
+waga = in(input())
+if waga <+ LIMIT:
+  print("JEDZ ze spokojem")
+else:
+  print("Lepiej nie jedz")
