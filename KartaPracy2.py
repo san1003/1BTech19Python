55a1f1b6afe062845f8582fd18052485f9402c88
1B6sh <114583306+san1003@users.noreply.github.com>
2022-10-12 12:14:59 +0000

    skonczenie kary pracy nr 2

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -14,12 +14,44 @@
 #    print("NIE")
 
 # Zad 3
+# wiek=int(input())
 
+# if wiek <=18:
+#   print("TAK jesteś pełnoletni")
+#   else 
+# print("NIE jesteś pełnoletni")
 
 # Zad 4
-LIMIT = 20
-waga = in(input())
-if waga <+ LIMIT:
-  print("JEDZ ze spokojem")
+#  LIMIT = 20
+# waga = int(input())
+# if waga <= LIMIT:
+#   print("JEDZ ze spokojem")
+# else:
+#   print("NIE jedz")
+
+#Zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#  if (c>a and c>b) or (c>b and c<a):  #a<b<c or b<c<a
+#    print("YES")
+#  else:
+#    print("NO") 
+
+#Zad 6
+# a = int(input())
+# p = int(input())
+
+# if (a**p-a) % p == 0:
+#   print("TAK, spelnia warunek mtf")
+# else:
+#   print("NIE nie spelnia warunku mtf")
+
+#Zad 7
+p = int(input())
+k = int(input())
+s = int(input())
+if ((p+k+s)/3 >= 0):
+  print("TAK")
 else:
-  print("Lepiej nie jedz")
+  print("NIE")
