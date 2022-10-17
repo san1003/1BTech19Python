56c74320e016f200a5c094be8ea15af5b620adc5
1B6sh <114583306+san1003@users.noreply.github.com>
2022-10-17 10:20:00 +0000

    zaczęto pre

---
+++ KartaPracy3.py
@@ -0,0 +1,13 @@
+#PRE
+# for i in range(10, 22):
+#   print(i)
+
+# pętla liczb dwucyfrowych od 10 do 21
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+for i in range(15,33,2):
+  print(i)
+  
