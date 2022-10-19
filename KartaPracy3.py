98094a72a6fd6c45f665920b1b8880363525fa4f
1B6sh <114583306+san1003@users.noreply.github.com>
2022-10-19 12:16:01 +0000

    .

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,13 +1,19 @@
-#PRE
-# for i in range(10, 22):
-#   print(i)
+#Zad 1
+# n = int(input())
 
-# pętla liczb dwucyfrowych od 10 do 21
-# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
-# pętla liczb dwucyfrowych malejąco (step ujemny)
-# pętla liczb dwucyfrowych malejąco (step dodatni)
-# pętla liczb 3-cyfrowych podzielnych przez 20
+# for i in range(n):
+#   print(i**3 + 3, end=" ")
 
-for i in range(15,33,2):
-  print(i)
+#Zad 2
+for i in range(105,1000,15):
+  print(i, end=" ")
+
+for i in range(100,1000):
+  if i % 15 == 0:
+    print(i, end=" ")
+    
+  
+
+
+
   
---
+++ pre.py
@@ -0,0 +1,29 @@
+#PRE
+# for i in range(10, 22):
+#   print(i)
+# print(list(range(10)))
+
+# pętla liczb dwucyfrowych od 10 do 21
+# for i in range(10,22):
+#   print(i, end=" ")
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31
+# for i in range(15,32,2):
+#   print(i, end=" ")
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+ #  for i in range(10, 100, 1):
+ # print(109-i, end=" ")
+# pętla liczb 3-cyfrowych podzielnych przez 20
+# for i in range(100, 1000, 20):
+#   print(i, end=" ")
+# print()
+# print()
+#   for i in range(5, 50):
+#     print(i*20, end=" ")
+# for i in range(99,9,-1):
+#   print(i, end=" ")
+
+# for i in range(99,9,1):
+#   print(i, end=" ")
+  
+  
