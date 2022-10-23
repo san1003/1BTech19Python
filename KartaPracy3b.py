d1b1c7ce433d5ba49f9c7c2b253f4091718c4fc4
1B6sh <114583306+san1003@users.noreply.github.com>
2022-10-23 15:18:12 +0000

    zrobiono 4 zadania

---
+++ KartaPracy3b.py
@@ -0,0 +1,20 @@
+#Zad1
+# for i in range(1,31,1):
+#   print(i, end=" ")
+
+#Zad2
+# for i in range(1, 10, 2):
+#   print(i**2)
+
+#Zad3
+# for i in range(1137, 10000, 379):
+#   print(i)
+
+#Zad4
+# for (i) in range(100, 1000):
+#   if (i%5 ==0 and i%6 ==0) or (i%5 ==0 and i%6 ==0 and i%6 == 0):
+#     print(i)
+
+#Zad5
+ 
+  
