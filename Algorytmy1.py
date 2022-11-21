5899efd600a92eb27a1378c1ba25258c6733d4b7
1B6sh <114583306+san1003@users.noreply.github.com>
2022-11-21 11:19:47 +0000

    zrobiono 2 punkt

--- Algorytmy1.py
+++ Algorytmy1.py
@@ -3,14 +3,22 @@
 # 2 3 5 7 11 13 17 19 23 itd
 #dzielniki wlasciwe- dzielniki liczby poza 1 i nia samą
 
-n= int(input())
-
-for i in range(2,n):
-  if n % i == 0:
-    print("liczba nie jest pierwsza")
-    exit(0)
-print("Liczba jest pierwsza")
-
-
+# n= int(input())
+# for i in range(2,n):
+#   if n % i == 0:
+#     print("liczba nie jest pierwsza")
+#     exit(0)
+# print("Liczba jest pierwsza")
 
 #2. generowanie liczb pierwszych
+
+# n= int(input("podaj do ilu mam szukać liczb pierwszych"))
+# for i in range(2, n+1):
+#   flaga = True;
+#   for j in range(2,i):
+#     if i % j == 0:
+#       flaga = False
+#       break
+#   if flaga:
+#      print(i, end=" ")
+          
