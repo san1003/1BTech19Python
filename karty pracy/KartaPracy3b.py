3aca7cb8745cc7304f47f6d7ccdecbe1db534784
1B6sh <114583306+san1003@users.noreply.github.com>
2022-12-18 20:22:20 +0000

    powtorzenie karty pracy 3 do oceny

--- karty pracy/KartaPracy3b.py
+++ karty pracy/KartaPracy3b.py
@@ -7,9 +7,10 @@
 #   print(i**2)
 
 #Zad3
-# for i in range(1137, 10000, 379):
-#   print(i)
-
+# for i in range(100,1000):
+#   if i%379==0:
+#    print(i)
+    
 #Zad4
 # for i in range(100, 1000):
 #   if (i%5 ==0 and i%6 ==0) or (i%5 ==0 and i%6 ==0 and i%11 == 0):
@@ -17,12 +18,12 @@
 
 #Zad5
 # n = int(input("w ile gramy?"))
-# suma = n * (n+1) // 2
-# for i in range(n-1):
-#    a = int(input())
-#    suma = suma - a
-# print(suma) 
-
+# suma=0
+# for i in range(n):
+#   liczba = int(input())
+#   suma += liczba
+# print(suma)  
+  
 
 #Zad6
 # k = int(input())
@@ -39,14 +40,26 @@
 # print(suma, end="")
 
 #Zad8
+# wartosc=int(input("podaj wartosc inwestycji"))
+# lata = int(input("podaj lata inwestycji"))
+# kap=0
+# suma=wartosc
+# for i in range(0,lata*12):
+#   kap=suma*0.06 *(1/12)
+#   suma += kap
+# print(f"koncowy kapital wynosi:{round(suma/2)}zł")
 
 #Zad9
 # n = int(input())
 # suma = 0
-# for i in range(21,n,100):
-#   suma += i 
+# for i in range(n):
+#   suma += (100*i)+21
 # print(suma)
 
-#Zad10
-for i in range (1,1000):
-  print(i)
+# #Zad10
+# from cmath import sqrt
+# for i in range(1,1001):
+#   if i%10 == sqrt(i):
+#     print(i)
+#   elif i%100 == sqrt(i):
+#     print(i)
