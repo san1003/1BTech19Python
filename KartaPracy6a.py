6bfef15d9ae055d645daf9e017e5e2c1430a90fe
1B6sh <114583306+san1003@users.noreply.github.com>
2023-01-15 13:25:11 +0000

    zadanie 3

---
+++ KartaPracy6a.py
@@ -0,0 +1,32 @@
+#Zad 1
+# x=int(input())
+# suma=0
+# while x > 0:
+#   cyfra=x % 10
+#   suma = suma + cyfra
+#   x = x // 10
+# print(suma)  
+
+# 367 % 10 = 7
+# 367 // 10 = 36
+
+#Zad2
+# a = int(input())
+
+# if a % a==0 and a % 1==0:
+#   print("jest liczba pierwsza")
+# else:
+#   print("nie jest liczba pierwsza")
+
+#Zad3
+# a = int(input())
+# suma = 0
+# for i in range(1,a):
+#     if a % i == 0:
+#         suma = suma + i
+# if suma == a:
+#   print("liczba jest doskonala")
+# else:
+#   print("liczba nie jest doskonala")
+
+#Zad4
