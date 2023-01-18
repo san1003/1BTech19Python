b1c9444df648472a619409ed1c1b00839ef1eab8
1B6sh <114583306+san1003@users.noreply.github.com>
2023-01-18 13:13:44 +0000

    lista

---
+++ 1.py
@@ -0,0 +1,28 @@
+
+# suma = 0
+# for i in range(10,100):
+#   suma = suma + i
+# print(suma)
+
+#pseudokod
+# suma = 0
+# dla kazdego i =11,12,13..99 wykonuj
+#   suma = suma + i
+# wypisz suma
+
+#zapisz program ktory k suma liczb wpisany przez usera
+# k = int(input())
+# suma = 0
+# for i in range(k):
+#   x = int(input())
+#   suma = suma + x
+# print(suma)  
+
+#pseudokod
+#czytaj k
+# suma = 0
+# dla kazdego i=1,2,...k wykonuj
+#    czytaj x
+#    suma = suma + x
+# wypisz suma
+
---
+++ lista.py
@@ -0,0 +1,14 @@
+L = [3,7,6,1,3,3,5,4,]
+print(L)
+print(L[1])
+print(L[6])
+print(len(L))
+
+for elem in L:
+    print(elem, end=" ")
+print("/n") 
+
+for i in range(len[L]):
+  print([L], end=" ")
+print("/n")  
+          
