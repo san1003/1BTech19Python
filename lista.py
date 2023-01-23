a4c02d9970194939957f88341e039cf15c67cf91
1B6sh <114583306+san1003@users.noreply.github.com>
2023-01-23 11:20:24 +0000

    zadanie do 4

--- lista.py
+++ lista.py
@@ -1,14 +1,40 @@
-L = [3,7,6,1,3,3,5,4,]
+# L = [3,7,6,1,3,3,5,4,]
+
+# print(L)
+# print(L[1])
+# print(L[6])
+# print(len(L))
+
+# for elem in L:
+#     print(elem, end=" ")
+# print("/n") 
+
+# for i in range(len[L]):
+#   print([L], end=" ")
+# print("/n")  
+
+# 1. Znajdź największą liczbę w tablicy
+# 2. Znajdź najmniejszą liczbę w tablicy
+# 3. Podaj ile razy występuje najwieksza liczba w tablicy
+# 4. Podaj ile razy występuje najmniejsza liczba w tablicy
+# 5. Podaj rozpiętość tablicy (różnica max - min)
+# 6. Podaj sumę liczb w tablicy
+# 7. Podaj średnią wartość liczb w tablicy
+# 8. Których liczb jest więcej w tablicy: parzystych czy nieparzystych?
+# 9. Ile w tablicy jest liczb pierwszych?
+# 10. Podaj v-ce max i v-ce min liczb tablicy
+
+from random import randint
+L = [randint(1,21) for i in range(20)]
 print(L)
-print(L[1])
-print(L[6])
-print(len(L))
+# zadanie 1
+print(max(L))
 
-for elem in L:
-    print(elem, end=" ")
-print("/n") 
+# zadanie 2
+print(min(L))
 
-for i in range(len[L]):
-  print([L], end=" ")
-print("/n")  
-          
+# zadanie 3
+print(L.count(max(L)))
+
+# zadanie 4
+print(L.count(min(L)))
