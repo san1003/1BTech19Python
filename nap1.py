49950f3679c4c2973ad270e2f5ec694e7f5006a0
1B6sh <114583306+san1003@users.noreply.github.com>
2023-03-01 13:14:21 +0000

    napis

---
+++ nap1.py
@@ -0,0 +1,44 @@
+#napisy (stingi) są "prawie listami"
+
+# s = input()
+# print(s)
+
+# for x in s:
+#   print(x)
+
+# for i in range(len(s)):
+#   print(s[i])
+
+# L =[5, 7, 1, 4]
+# print(s, L)
+# L.sort()
+# print(s,L)
+# s.sort()  ---> to nie działa błąd
+# print(s,L)
+
+#konwersja list <-> napis (list(),jest
+
+# s = input()
+# L = list(s)
+# print(s,L)
+# L.sort()
+# print(s,L)
+
+#
+s = input()
+L = list(s)
+print(s, L)
+L.sort()
+s = "".join(L)
+print(s, L)
+
+#sprawdz czy napis jest palindromem
+s = input()
+L = list(s)
+R = L.copy()
+R.reverse()
+if L == R:
+  print("tak")
+else:
+  print("nie")
+  
